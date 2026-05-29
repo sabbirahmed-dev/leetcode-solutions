4ac0919bf3f8bb14ae7a7b8ca15ccd83d08844c5
Sabbir Ahmed Abir <abirbinhossain.sa@gmail.com>
2026-05-29 20:22:42 +0600

    Problem-23 Solution

---
+++ Leetcode Solutions/023_MergeKSortedLists.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> minHeap =
+                new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                minHeap.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!minHeap.isEmpty()) {
+            ListNode smallest = minHeap.poll();
+
+            current.next = smallest;
+            current = current.next;
+
+            if (smallest.next != null) {
+                minHeap.offer(smallest.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
